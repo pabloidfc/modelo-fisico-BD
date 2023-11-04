c85f9c7f855b31d0917d5a5c1bece092c2f4128f
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-11-04 13:17:14 -0300

    agregando usuarios y vehiculos como datos dummie

--- datos_prueba.sql
+++ datos_prueba.sql
@@ -2,12 +2,22 @@ use santiago_etchebarne;
 
 insert into users (ci, nombre, apellido, apellido2, email, password)
     values
-        ("54345671", "Pablo", "King", "Kong", "admin@admin.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
-        ("34345678", "Federico", "King", "Kong", "ferkinkong@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
-        ("23213678", "Alberto", "Fernandez", "Fernandez", "alferfer@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("54345671", "Lionel", "Messi", "Cuccittini", "admin@admin.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("34345678", "Raúl", "Álvarez", "Genes", "aurongaming@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("23213678", "Guillermo", "Díaz", "Ibáñez", "madremiawilly@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
         ("56789054", "Mirta", "Legrand", "Gon calvez", "mirtalegrand@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
-        ("56789045", "Enirique", "Cleptomano", "Sole", "eniruqe@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
-        ("54434566", "Milei", "Milanga", "Milanguesa", "2020jajasalu2@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y");
+        ("56789045", "Iván", "Raúl", "Buhajeruk", "spreeen@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("34323456", "Samuel", "de Luque", "Batuecas", "v777@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("56789025", "Ibai", "Llanos", "Garatea", "ibaicito@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("56789145", "Rubén", "Doblas", "Gundersen", "rubiuh@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("56729045", "Germán", "Garmendia", "Aranís", "germengermendia@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("56783045", "Martín", "Pérez", "Disalvo", "coscu@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("54434566", "Mario", "Alonso", "Gallardo", "djmarito@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("54424566", "Alejandro", "Bravo", "Yañez", "11alexby@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("54414566", "Geronimo", "Benavidez", "Benavidez", "elmomo@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("52234566", "Javier", "Gerardo", "Milei", "libertad@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("53334566", "Daniel", "Santomé", "Lemus", "dalasreview@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y"),
+        ("54573566", "Gonzálo", "Banzas", "Banzas", "gonchito@gmail.com", "$2y$10$DfhgfGk9VSPe/O///qg5iuhmT7b9o7s8.Xhrzq3LTt20c6jvocs9y");
 
 insert into cliente(rut,direccion,email,cuentabancaria)
     values
@@ -46,7 +56,17 @@ insert into funcionario(user_id,almacen_id,tipo,empresa_id)
     values
         (2,1,"Propio",null),
         (3,1,"Propio",null),
-        (4,5,"De terceros",1);
+        (4,5,"De terceros",1),
+        (7,1,"Propio",null),
+        (8,1,"Propio",null),
+        (9,1,"Propio",null),
+        (10,1,"Propio",null),
+        (11,1,"Propio",null),
+        (12,1,"Propio",null),
+        (13,1,"Propio",null),
+        (14,1,"Propio",null),
+        (16,1,"Propio",null),
+        (15,1,"Propio",null);
 
 insert into administrador(user_id)
     values
@@ -56,3 +76,10 @@ insert into transportista(user_id)
     values
         (5),
         (6);
+
+insert into vehiculo (matricula,peso,limite_peso)
+    values
+        ("abc1234",4000,22000),
+        ("abc2234",4000,22000),
+        ("abc3234",4000,22000),
+        ("abc4234",4000,22000);
